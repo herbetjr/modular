2cc8c79773cf2578aaec8952c736439eb4de9248
herbetjr <herbetjr@gmail.com>
2019-04-17 23:13:04 -0300

    primeiro commit
    commmit one

---
+++ Modules/Clientes/Resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('clientes::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('clientes.name') !!}
+    </p>
+
+@stop
